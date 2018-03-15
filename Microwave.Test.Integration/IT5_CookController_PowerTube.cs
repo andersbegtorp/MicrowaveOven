b9695cc864c2fdfcda3cb173da3d08b60fe48bdc
ChristianMorup <chrmorup@gmail.com>
2018-03-15 12:35:43 +0100

    CookController is finished

--- Microwave.Test.Integration/IT5_CookController_PowerTube.cs
+++ Microwave.Test.Integration/IT5_CookController_PowerTube.cs
@@ -38,9 +38,31 @@ namespace Microwave.Test.Integration
         public void StartCooking_OutputShowsPower_PowerIsCorrect(int power, int time)
         {
             _cookController.StartCooking(power, time);
-            _output.OutputLine(Arg.Is<string>(str => str.Contains(power +" %")));
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains(power +" %")));
         }
 
+        [Test]
+        public void Stop_OutputShowsPowerStatus_PowerIsTurnedOff()
+        {
+            _cookController.Stop();
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        [Test]
+        public void OnTimerExpired_IsCooking_OutputShowsPowerTubeIsTurnedOff()
+        {
+            _cookController.StartCooking(20, 10);
+            _output.ClearReceivedCalls();
+            _cookController.OnTimerExpired(_timer, EventArgs.Empty);
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        [Test]
+        public void OnTimerExpired_IsNotCooking_OutputDidNotRecieveAnyCalls()
+        {
+            _cookController.OnTimerExpired(_timer, EventArgs.Empty);
+            _output.DidNotReceive().OutputLine(Arg.Any<string>());
+        }
 
     }
 }
